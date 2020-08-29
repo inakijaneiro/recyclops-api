bf97187ce0e933fc7b52a8ad3d484a1f79587743
Iñaki Janeiro Olague <inaki_janeiro@live.com.mx>
2020-08-29 16:59:34 -0500

    Fix package.json for prod

--- db/index.js
+++ db/index.js
@@ -24,26 +24,7 @@ mongoose.connect(`mongodb://${dbUser}:${dbPassword}@ds058739.mlab.com:58739/recy
 
     console.log("Connection successful!")
 
-    let x = new Product({
-        name: "Té", 
-        description: "Té de manzanilla McCormick ay no Hellman's que bruto",
-        wasteComposition: [
-            {
-                name: "Papel",
-                description: "Pos papel we",
-                timeToLive: 1.5
-            },
-            {
-                name: "Orgánico",
-                description: "Hojas deshidratadas",
-                timeToLive: 0.1
-            }
-        ],
-        instructions: "Lo abres lo separas y lo tiras :D en el mismo vale madres",
-        gtin: 1234,
 
-    })
-    // x.save().then(() => console.log("Se añadio la madre esta"))
 }
 );
 
